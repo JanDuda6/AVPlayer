7a74ad7ea3dafb200baca946eb2e5280f225eba9
Jan Duda <jan.duda6@gmail.com>
2021-09-01 14:38:15 +0200

    init commit

---
+++ AVPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayer
+//
+//  Created by Kurs on 01/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
